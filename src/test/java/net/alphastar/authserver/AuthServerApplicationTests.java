29e95975679faa3a2218ddb28174861829d48b4b
Rohan Truscott <r.truscott@gmail.com>
2022-03-05 13:10:56 +1100

    Initial commit

---
+++ src/test/java/net/alphastar/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.alphastar.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
